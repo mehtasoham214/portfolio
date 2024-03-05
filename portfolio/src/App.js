156707416ab201dd91e224117d05a73a0d91dd50
mehtasoham214 <mehtasoham214@gmail.com>
2024-03-05 10:35:38 -0500

    Added a loader fixed navbar alignment

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,18 +8,55 @@ import {
     Works,
 } from "./container";
 import { Navbar } from "./components";
+import { HashLoader } from "react-spinners";
+import { useState, useEffect } from "react";
+import "./App.scss";
+import { motion } from "framer-motion";
+
+const override = {
+    display: "block",
+    margin: "0 auto",
+};
 
 const App = () => {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 2500);
+    }, []);
     return (
-        <div>
-            <Navbar />
-            <Header />
-            <About />
-            <Skills />
-            <Works />
-            <Testimonials />
-            <Footer />
-        </div>
+        <>
+            {loading ? (
+                <div className="loading">
+                    <motion.div
+                        initial={{ x: -70 }}
+                        animate={{ x: 0 }}
+                        transition={{ type: "spring", stiffness: 100 }}
+                    >
+                        <HashLoader
+                            color="#3945c2"
+                            loading={loading}
+                            cssOverride={override}
+                            size={90}
+                            aria-label="Ring Spinner"
+                            data-testid="ring-spinner"
+                        />
+                    </motion.div>
+                </div>
+            ) : (
+                <div>
+                    <Navbar />
+                    <Header />
+                    <About />
+                    <Skills />
+                    <Works />
+                    <Testimonials />
+                    <Footer />
+                </div>
+            )}
+        </>
     );
 };
 
